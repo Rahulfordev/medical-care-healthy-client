b0e2560fe57951257a09019f93651b9e44140b2f
rahulfordev <rahulforsms@gmail.com>
2024-05-15 22:23:36 +0600

    dashboard design change

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -14,12 +14,12 @@ function Sidebar() {
   return (
     <div className="drawer lg:drawer-open max-w-7xl mx-auto z-40">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content  flex flex-col items-center justify-center">
+      <div className="drawer-content group flex flex-col items-center justify-center">
         <label
           htmlFor="my-drawer-2"
-          className="hover:bg-blue-700 ml-8 btn bg-transparent border-none drawer-button lg:hidden"
+          className="group-hover:bg-[#e12454] ml-8 btn bg-transparent border-none drawer-button lg:hidden"
         >
-          <FiAlignLeft className="text-2xl text-black hover:text-white" />
+          <FiAlignLeft className="text-2xl text-black group-hover:text-white" />
         </label>
       </div>
       <div className="drawer-side">
@@ -28,9 +28,9 @@ function Sidebar() {
           aria-label="close sidebar"
           className="drawer-overlay"
         ></label>
-        <ul className="font-custom text-base menu pt-14 lg:pt-4 lg:mx-4 w-60 lg:rounded-md min-h-full lg:min-h-[90%] bg-blue-700 text-white">
+        <ul className="font-custom text-base menu pt-14 lg:pt-4 lg:mx-4 w-60 lg:rounded-md min-h-full lg:min-h-[90%] bg-[#8fb569] text-white">
           <li>
-            <Link to="/dashboard">
+            <Link to="/dashboard" className="text-white">
               <FaRegClock /> My Appointments
             </Link>
           </li>
--- src/pages/Home/OurInfo/OurInfo.jsx
+++ src/pages/Home/OurInfo/OurInfo.jsx
@@ -5,56 +5,58 @@ import { BsFillTelephoneOutboundFill } from "react-icons/bs";
 const OurInfo = () => {
   return (
     <div className="mt-[70px] max-w-7xl mx-auto">
-      
       <div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mx-5">
           {/* part 1 */}
-          <div className="bg-orange-600 p-12 rounded-lg">
-            <div className="flex gap-3">
-              <div className="text-white mt-1">
-                <HiOutlineClock size={32}></HiOutlineClock>
+          <div className="pt-10 rounded-lg border border-[#eaedff]">
+            <div className="text-center flex flex-col items-center mb-10">
+              <div className="mt-1 flex items-center justify-center bg-[#8fb569] rounded-full w-36 h-36 shadow-md border-8 border-white mb-5">
+                <HiOutlineClock
+                  size={50}
+                  className="text-white"
+                ></HiOutlineClock>
               </div>
               <div>
-                <h6 className="text-[25px] font-bold text-white m-0">
+                <h6 className="text-[25px] font-bold text-[#223645]">
                   Opening Hours
                 </h6>
-                <p className="text-white text-xl">
+                <p className="text-base text-[#777c94]">
                   Open 10.00 am to 4.00pm Everyday
                 </p>
               </div>
             </div>
           </div>
           {/* part 2 */}
-          <div className="bg-green-700 p-12 rounded-lg">
-            <div className="flex gap-3">
-              <div className="text-white mt-1">
-                <MdLocationPin size={32}></MdLocationPin>
+          <div className="pt-10 rounded-lg border border-[#eaedff]">
+            <div className="text-center flex flex-col items-center mb-10">
+              <div className="mt-1 flex items-center justify-center bg-[#8fb569] rounded-full w-36 h-36 shadow-md border-8 border-white mb-5">
+                <MdLocationPin size={50} className="text-white"></MdLocationPin>
               </div>
               <div>
-                <h6 className="text-[25px] font-bold text-white m-0">
+                <h6 className="text-[25px] font-bold text-[#223645]">
                   Our Locations
                 </h6>
-                <p className="text-white text-xl">
+                <p className="text-base text-[#777c94]">
                   Chennai 120, Chennai -5000, India
                 </p>
               </div>
             </div>
           </div>
           {/* part 3 */}
-          <div className="bg-fuchsia-600 p-12 rounded-lg">
-            <div className="flex gap-3">
-              <div className="text-white mt-2">
+          <div className="pt-10 rounded-lg border border-[#eaedff]">
+            <div className="text-center flex flex-col items-center mb-10">
+              <div className="mt-1 flex items-center justify-center bg-[#8fb569] rounded-full w-36 h-36 shadow-md border-8 border-white mb-5">
                 <BsFillTelephoneOutboundFill
-                  size={32}
+                  size={50}
+                  className="text-white"
                 ></BsFillTelephoneOutboundFill>
               </div>
               <div>
-                <h6 className="text-[25px] font-bold text-white m-0">
+                <h6 className="text-[25px] font-bold text-[#223645]">
                   Contact Us
                 </h6>
-                <p className="text-white text-xl">
-                  +91 22 1234 5678 <br />
-                  +91 22 1234 5678
+                <p className="text-base text-[#777c94]">
+                  +91 22 1234 5678 <br /> +91 22 1234 5678
                 </p>
               </div>
             </div>
